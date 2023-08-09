453e8c39344fb1e232f05563e62d558c4ec5f800
Sean Gresham <sean.gresham@keptcompanies.com>
2023-08-09 00:20:34 -0700

    stable through infinite scrolling

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,33 +1,64 @@
 import Link from "next/link";
-import { signOut,signIn, useSession } from "next-auth/react";
+import { signOut, signIn, useSession } from "next-auth/react";
+import { IconHoverEffect } from "./IconHoverEffect";
+import { VscAccount, VscHome, VscSignIn, VscSignOut } from "react-icons/vsc";
 
-
-export function SideNav(){
-    const session = useSession()
-    const user = session.data?.user
-    return (
+export function SideNav() {
+  const session = useSession();
+  const user = session.data?.user;
+  return (
     <nav className="sticky-top-0 px-2 py-4">
-        <ul className="flex flex-col items-start gap-2 shitespace-nowrap">
-            <li>
-                <Link href="/">Home</Link>
-            </li>
-            {user != null && (
-                <li>
-                    <Link href={`/profiles/${user.id}`}>Profile</Link>
-                </li>
-            )}
-            {user == null ? (
-                <li>
-                    <button onClick={()=> void signIn()}>Login</button>
-                </li>
-
-            ) : (
-                <li>
-                    <button onClick={()=>void signOut()}>Logout</button>
-                </li>
-           )}
-        </ul>
-        
+      <ul className="shitespace-nowrap flex flex-col items-start gap-2">
+        <li>
+          <Link href="/">
+            <IconHoverEffect>
+              <span className="flex items-center gap-4">
+                <VscHome className="h-8 w-8" />
+                <span className="test-lg hidden md:inline">Home</span>
+              </span>
+            </IconHoverEffect>
+          </Link>
+        </li>
+        {user != null && (
+          <li>
+            <Link href={`/profiles/${user.id}`}>
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscAccount className="h-8 w-8" />
+                  <span className="test-lg hidden md:inline">Profile</span>
+                </span>
+              </IconHoverEffect>
+            </Link>
+          </li>
+        )}
+        {user == null ? (
+          <li>
+            <button onClick={() => void signIn()}>
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscSignIn className="h-8 w-8 fill-green-700" />
+                  <span className="test-lg hidden text-green-700 md:inline">
+                    Login
+                  </span>
+                </span>
+              </IconHoverEffect>
+            </button>
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => void signOut()}>
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscSignOut className="h-8 w-8 fill-red-700" />
+                  <span className="test-lg hidden text-red-700 md:inline">
+                    Logout
+                  </span>
+                </span>
+              </IconHoverEffect>
+            </button>
+          </li>
+        )}
+      </ul>
     </nav>
-    );
+  );
 }
